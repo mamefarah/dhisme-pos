812877bd29d63790fa8ec6b762ea29c819721776
Muhammed Farah <93242587+mamefarah@users.noreply.github.com>
2026-06-17 04:18:14 +0300

    rename app title

--- lib/app.dart
+++ lib/app.dart
@@ -11,7 +11,7 @@ class DhismePosApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Dhisme POS',
+      title: 'Dukaan Dhisme POS',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.light(),
       home: isConfigured ? const AuthGate() : const ConfigMissingScreen(),
