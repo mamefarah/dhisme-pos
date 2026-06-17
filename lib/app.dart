def553eeede60247b9b64e880cc6dc7603a70d61
Muhammed Farah <93242587+mamefarah@users.noreply.github.com>
2026-06-17 04:25:52 +0300

    wire language switch into app

--- lib/app.dart
+++ lib/app.dart
@@ -1,20 +1,35 @@
 import 'package:flutter/material.dart';
+import 'core/i18n/app_language.dart';
 import 'core/theme/app_theme.dart';
 import 'features/auth/screens/auth_gate.dart';
 import 'features/auth/screens/config_missing_screen.dart';
 
-class DhismePosApp extends StatelessWidget {
+class DhismePosApp extends StatefulWidget {
   const DhismePosApp({super.key, required this.isConfigured});
 
   final bool isConfigured;
 
   @override
+  State<DhismePosApp> createState() => _DhismePosAppState();
+}
+
+class _DhismePosAppState extends State<DhismePosApp> {
+  @override
+  void initState() {
+    super.initState();
+    AppLanguage.instance.load();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Dukaan Dhisme POS',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.light(),
-      home: isConfigured ? const AuthGate() : const ConfigMissingScreen(),
+    return AnimatedBuilder(
+      animation: AppLanguage.instance,
+      builder: (context, _) => MaterialApp(
+        title: 'Dukaan Dhisme POS',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.light(),
+        home: widget.isConfigured ? const AuthGate() : const ConfigMissingScreen(),
+      ),
     );
   }
 }
